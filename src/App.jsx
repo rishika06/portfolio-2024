d1ba2c7d5fc682e3a50df8b94b0ab54ed2bfd7d1
rishika06 <rishika2306@gmail.com>
2024-12-30 11:45:39 +0530

    alignment fixes

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ function App() {
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
       <div className="lg:flex lg:justify-between lg:gap-4">
-        <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+        <header className="lg:sticky lg:top-0 flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24 justify-center">
           <Hero />
         </header>
-        <main className="lg:w-1/2 lg:py-24">
+        <main className="lg:w-1/2 lg:py-24 pl-4">
           <About />
           <Experience />
           <Projects />
